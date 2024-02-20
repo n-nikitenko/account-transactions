c8d4a52dbad83c1040e17efbfa7e96a2b737df72
nnikitenko <ninanineliya@gmail.com>
2024-02-20 15:28:13 +0300

    Добавлена функция получения последних пяти операций, отсортированных в порядке убывания значения

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import json
 import os.path
+from datetime import datetime
 
 
 def get_json_data():
@@ -8,3 +9,14 @@ def get_json_data():
         data = json.load(json_file)
         return data
 
+
+def get_last_five_operations(json_data):
+    """
+        Принимает список словарей, содержащих данные по операциям.
+        Каждый словарь обязательно должен содержать поле 'date', иначе он не будет обработан.
+        возвращает список, который содержит последние пять операций, отсортированный
+        в порядке убывания значения поля 'date'
+     """
+    operations = [operation for operation in json_data if 'date' in operation]
+    operations.sort(key=lambda operation: datetime.strptime((operation['date']), "%Y-%m-%dT%H:%M:%S.%f"), reverse=True)
+    return operations[:5]
