1c9ac96da295e0f021f21de95082d2e1713c1905
nnikitenko <ninanineliya@gmail.com>
2024-02-20 15:49:12 +0300

    Изменена функция получения последних пяти операций:
    теперь возвращаются только выполненные операции (у которых state=="EXECUTED")

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import json
 import os.path
 from datetime import datetime
+from pprint import pprint
 
 
 def get_json_data():
@@ -12,11 +13,20 @@ def get_json_data():
 
 def get_last_five_operations(json_data):
     """
-        Принимает список словарей, содержащих данные по операциям.
+        Принимает список словарей, содержащих данные по выполненным операциям.
         Каждый словарь обязательно должен содержать поле 'date', иначе он не будет обработан.
-        возвращает список, который содержит последние пять операций, отсортированный
+        Возвращает список, который содержит последние пять выполненных операций, отсортированный
         в порядке убывания значения поля 'date'
      """
-    operations = [operation for operation in json_data if 'date' in operation]
-    operations.sort(key=lambda operation: datetime.strptime((operation['date']), "%Y-%m-%dT%H:%M:%S.%f"), reverse=True)
-    return operations[:5]
+    op_count = 5
+    status = 'EXECUTED'
+    date_field = 'date'
+    state_field = 'state'
+    dt_format = '%Y-%m-%dT%H:%M:%S.%f'
+
+    operations = [operation for operation in json_data if date_field in operation]
+    operations.sort(key=lambda operation: datetime.strptime((operation[date_field]), dt_format), reverse=True)
+    with_status = list(filter(lambda operation: operation[state_field] == status, operations))
+
+    return with_status[:op_count if op_count < len(with_status) else len(with_status)]
+
